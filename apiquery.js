1ec95b6a0a328aa6f7b59d48bf7c28a8b8a76316
Richard King <mail@richardskingdom.net>
2014-12-01 22:47:30 +0100

    Make a real API call

--- apiquery.js
+++ apiquery.js
@@ -1,4 +1,19 @@
-var button = document.querySelector( "button" );
-button.addEventListener( "click", function( request ) {
-    alert( "Request happens here" );
+var button = document.querySelector("button");
+button.addEventListener("click", function(clicky) {
+    $.ajax({
+        url: "http://content.guardianapis.com/search",
+        type: "GET",
+        data: "api-key=test",
+        dataType: "json",
+        success: function(json) {
+            $( "#response" ).html("Number of pages: " + json.response.pages);
+        },
+        error: function(xhr, status, errorThrown) {
+            alert("Sorry, there was a problem!");
+            console.log("Error: " + errorThrown);
+            console.log("Status: " + status);
+            console.dir(xhr);
+        },
+    });
 }, false);
+
