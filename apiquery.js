61d225e5be8aca6b0f74b1dab2b0680d71024c75
Tom Barrett <tbtommyb@gmail.com>
2015-06-11 21:20:54 +0100

    added Discogs button code

--- apiquery.js
+++ apiquery.js
@@ -1,9 +1,15 @@
 // Hey, I thought it would be fun to make a project that recommended a record
 // based on the current headline, linking in with the Discogs API
+// The idea is that it searches the Discogs Marketplace using the first word of the
+// headline and returns the first result
+
+// STILL TO DO: Discogs auth to make that bit work. Also need to make a more
+// sophisticated way of choosing a record than just first word of headline
 
 var headlineButton = document.getElementById("headline-button");
 var headlineText = "";
 var firstWord = "";
+var resultRecord = "";
 headlineButton.addEventListener("click", function(clicky) {
     $.ajax({
         url: "http://content.guardianapis.com/search",
@@ -25,7 +31,24 @@ headlineButton.addEventListener("click", function(clicky) {
     });
 }, false);
 
-//var recordButton = document.getElementById("record-button");
-//recordButton.addEventListener("click", function(clicky) {
-//    $.ajax({
+var recordButton = document.getElementById("record-button");
+recordButton.addEventListener("click", function(clicky) {
+    $.ajax({
+        url: "https://api.discogs.com/database/search",
+        type: "GET",
+        data: "q=" + firstWord;
+        dataType: "json";
+        success: function(json) {
+            resultRecord = json.response.results[0].title;
+            $( "#record").html("You should buy: " + resultRecord);
+        },
+        error: function(xhr, status, errorThrown) {
+            alert("Sorry, there was a problem!");
+            console.log("Error: " + errorThrown);
+            console.log("Status: " + status);
+            console.dir(xhr);
+        },
+    });
+}, false);
+
         
