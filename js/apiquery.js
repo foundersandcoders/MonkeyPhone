e5398efd24aca1ce6ba9b9c37684f9aaeb15e6ed
Patrick Walkowicz <patrykwalkowicz@gmail.com>
2015-09-28 20:24:35 +0100

    Changed content rendering logic

--- js/apiquery.js
+++ js/apiquery.js
@@ -19,18 +19,14 @@ $(document).ready(function() {
 
                 // Content display part
                 // Display content
-
-                for (i = 1; i <= 10; i++) {   
-                    $('.news').append('<div class="news-item"><h3 class="news-item-font">' + json.response.results[i].webTitle + '</h3><p>' + json.response.results[i].fields.standfirst + '</p><p class="push-top push-bottom"><a class="btn btn-default" id="unique-id" role="button">View details &raquo;</a></p></div>');
+                var currentNewsItem = $('div.news-item').first();
+                var counter = 1;
+                while (counter < 10) {
+                    currentNewsItem.find('h3.news-item-font').append(json.response.results[counter].webTitle);
+                    currentNewsItem.find('p.news-snippet').append(json.response.results[counter].fields.standfirst);
+                    currentNewsItem = currentNewsItem.next();
+                    counter++;
                 }
-                console.log(newsItems);
-
-                // Query for first 10 entries
-                for (i = 1; i < 10; i++) {
-                    $('.content').append('<h2>' + json.response.results[i].webTitle + '</h2>');
-                    $('.content').append('<p class="snippet">' + json.response.results[i].fields.standfirst + '</p>');
-                }
-
             },
             error: function(xhr, status, errorThrown) {
                 alert("Sorry, there was a problem!");
@@ -41,41 +37,9 @@ $(document).ready(function() {
         });
     }
 
-    var showArticle = function(articleNumber) {
-    $('body div.content').append('<h1 class="jumbotron-headline">' + articleCache[articleNumber].title + '</h1>');
-    $('body div.content').append(articleCache[articleNumber].body);
-    }
 
-    var emptyContent = function() {
-        emptyTemp = $('body div.content').html();
-        $('body div.content').children().remove();
-    }
+// Functions to go here
 
-    var retreiveContent = function() {
-        $('body div.content').append(emptyTemp);
-    }
-
-    var hideItems = function() {
-        $('.row').hide();
-    }
-
-    var hideJumbotron = function() {
-        $('.jumbotron').hide();
-    }
-
-
-    generateMain();
-
-    $('.jumbotron .btn').click(function() {
-        emptyContent();
-        showArticle(0);
-
-    });
-
-    
-
-    
-    
 });
 
 
