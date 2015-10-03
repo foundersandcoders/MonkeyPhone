de4f91d7048f0bac94e7bc7cf6fdf091abc738f5
Patrick Walkowicz <patrykwalkowicz@gmail.com>
2015-10-03 13:33:24 +0100

    Added AngularJS views for home page and news items

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
-var app = angular.module('NewsApp', ['ngRoute']);
+var app = angular.module('NewsApp', ['ngRoute', 'ngSanitize']);
 app.config(function ($routeProvider) { 
   $routeProvider 
     .when('/', { 
       controller: 'HomeController', 
       templateUrl: 'views/home.html' 
-    }) 
+    })
+    .when('/news/:id', {
+    	controller: 'NewsController',
+    	templateUrl: 'views/news.html'
+    })
     .otherwise({ 
       redirectTo: '/' 
     }); 
