f18edcbe7b561d3649a1a6633f60ab351f89c7a4
Ivan Nieto <ivan.n.s@tuta.io>
2016-12-22 20:45:05 +0100

    Add background to Jumbotron calling The Guardian API

--- js/apiquery.js
+++ js/apiquery.js
@@ -2,22 +2,29 @@ $(document).ready(function() {
     $.ajax({
         url: "http://content.guardianapis.com/search",
         type: "GET",
-        data: "q=technology&show-fields=all&api-key=test",
+        data: "?q=technology&show-fields=all,starRating,headline,thumbnail,short-url&api-key=test",
         dataType: "json",
         success: function(json) {
             // Add the latest entry
-            $('#latest-headline').append(json.response.results[0].webTitle);
+            $('.jumbotron').css({
+                'background': 'linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8)), url(' + json.response.results[0].fields.thumbnail + ')',
+                'background-repeat': 'no-repeat, no-repeat',
+                'background-size': '100%',
+                'background-position': 'center'
+            });
+            $('#latest-headline').append(json.response.results[0].webTitle).css({'color': 'white', 'font-weight': 'bolder'});
             $('#latest-snippet').append(json.response.results[0].fields.standfirst);
+            $('.push-top .btn').attr('href', json.response.results[0].webUrl);
 
             // Content display part
             // Display content
             for (i = 1; i <= 10; i++) {
-                $('.news').append('<div class="news-item"><h3 class="news-item-font">' + json.response.results[i].webTitle + '</h3><p>' + json.response.results[i].fields.standfirst + '</p><p class="push-top push-bottom"><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p></div>');
+                $('.news').append('<div class="news-item"><h3 class="news-item-font">' + json.response.results[i].webTitle + '</h3><p>' + json.response.results[i].fields.standfirst + '</p><p class="push-top push-bottom"><a class="btn btn-default" href="'+json.response.results[i].webUrl+'" role="button">View details &raquo;</a></p></div>');
             }
 
             // Query for first 10 entries
             for (i = 1; i < 10; i++) {
-                $('.content').append('<h2>' + json.response.results[i].webTitle + '</h2>');
+                $('.content').append('<h2>'+ json.response.results[i].webTitle + '</h2>');
                 $('.content').append('<p class="snippet">' + json.response.results[i].fields.standfirst + '</p>');
             }
 
@@ -27,7 +34,6 @@ $(document).ready(function() {
             console.log("Error: " + errorThrown);
             console.log("Status: " + status);
             console.dir(xhr);
-        },
+        }
     });
 }, false);
-
